b82c34deba394ecc6ca39a42312c3f85c42a29bc
witt3rd <witt3rd@witt3rd.com>
2019-05-01 08:41:46 -0700

    remove graphqlconfig

---
+++ service/src/start.dev.js
@@ -0,0 +1,12 @@
+import '@babel/polyfill'
+import initServer from './server'
+import chalk from 'chalk'
+
+console.log(
+  chalk.bgRed('Authentication is disabled. This should not run in production')
+)
+
+initServer({
+  httpAuthMiddleware: false,
+  socketAuthMiddleware: false
+})
